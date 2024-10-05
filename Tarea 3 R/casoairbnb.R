f5b24dbe1eac669588cd0ad8d68621322d6526b5
jvliouc <julio.orozco@uc.cl>
2024-10-05 03:01:55 -0300

    avance, con cosas que solucionar

---
+++ Tarea 3 R/casoairbnb.R
@@ -0,0 +1,115 @@
+library(topicmodels)
+library(tm)
+library(wordcloud)
+library(ggplot2)
+library(syuzhet)
+library(readr)
+#------------------------------------------------------------------------------#
+#libreria extra que me pidio R y itra oara agregar color a los mapas
+library(RColorBrewer)
+library(dplyr)
+library(SnowballC)
+#------------------------------------------------------------------------------#
+#leer datos
+reviews  <- read_csv("Rtasks/Tarea 3 R/Miami_reviews.csv")
+listings <- read_csv("Rtasks/Tarea 3 R/Miami_listings.csv")
+#------------------------------------------------------------------------------#
+#copiando el codigo del cleansing del enunciado
+docs<-iconv(reviews$comments,to="UTF-8")# corrige caracteres especiales [,2]
+docs<-VCorpus(VectorSource(docs))# define un corpus o cuerpo en R para poder transformarlo
+docs<-tm_map(docs,content_transformer(tolower))# inicio comandos cleansing
+docs<-tm_map(docs,stripWhitespace)
+docs<-tm_map(docs,removePunctuation)
+docs<-tm_map(docs,removeNumbers)
+docs<-tm_map(docs,removeWords, stopwords("english"))#elimina conectores en ingles
+docs<-tm_map(docs, stemDocument, language = "en") # fin comandos cleansing
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#                             Pregunta 1
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#Calcule los indicadores TF, IDF y TFIDF en forma agregada (para todos los documentos)
+#mediante los siguientes comandos.
+docs2=DocumentTermMatrix(docs) # cuenta la frecuencia de cada palabra del corpus anterior
+m=as.matrix(docs2) #MATRIZ DE FRECUENCIAS
+TF=apply(m,2,sum)/dim(m)[1]  
+df=apply(m>0,2,mean)
+IDF=1+log10(1/df)
+#------------------------------------------------------------------------------#
+#grafico TF
+palabras1=names(TF)
+par(mar = c(1, 1, 1, 1))
+set.seed(10)
+wordcloud(words = palabras1,
+          freq = TF*100, 
+          min.freq = 1,      # Frecuencia mínima para mostrar una palabra
+          max.words = 50, # Máximo número de palabras en la nube
+          random.order = FALSE,  # Las palabras más frecuentes aparecen en el centro
+          rot.per = 0.35,     # Proporción de palabras que se rotan a 90 grados
+          scale = c(4, 0.5),
+          colors = brewer.pal(8, "Paired"))
+#------------------------------------------------------------------------------#
+#grafico TFIDF
+TFIDF=TF*IDF
+palabras2=names(TFIDF)
+par(mar = c(1, 1, 1, 1))
+set.seed(10)
+wordcloud(words = palabras2,
+          freq = TFIDF*100,
+          min.freq = 1,      # Frecuencia mínima para mostrar una palabra
+          max.words = 50, # Máximo número de palabras en la nube
+          random.order = FALSE,  # Las palabras más frecuentes aparecen en el centro
+          rot.per = 0.35,     # Proporción de palabras que se rotan a 90 grados
+          scale = c(4, 0.5),
+          colors = brewer.pal(8, "Paired"))
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#                             Pregunta 2
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+n=length(docs)
+docs1=rep(0,n)
+for (i in 1:n){docs1[i]=docs[[i]]$content}
+sentiment=get_sentiment(char_v=docs1, method="syuzhet")
+sentiment_df <- data.frame(sentiment = sentiment)
+
+# Crear un histograma con ggplot2
+ggplot(sentiment_df, aes(x = sentiment)) +
+  geom_histogram(binwidth = 0.5, fill = "lightblue", color = "black") +
+  labs(title = "Distribución de los Puntajes de Sentimiento",
+       x = "Puntaje de Sentimiento",
+       y = "Frecuencia") +
+  theme_minimal()
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#                             Pregunta 3
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+
+listings<-listings%>%
+  mutate(Sentiment=sentiment)%>%
+  mutate(log_occupancy1=log(occupancy+1))%>%
+  mutate(log_price=log(price))
+
+modelo_con_sentiment<-lm(log_occupancy1~log_price+Sentiment+rating,listings)
+summary(modelo_con_sentiment)
+modelo_sin_sentiment<-lm(log_occupancy1~log_price+rating,listings)
+summary(modelo_sin_sentiment)
+
+
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#                             Pregunta 4
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+docs2=DocumentTermMatrix(docs) #calcula la matrix de términos
+docs3=removeSparseTerms(docs2, .99) #elimina palabras con baja frecuencia
+sumr=rowSums(as.matrix(docs3)) #elimina documentos con cero palabras
+docs3=docs3[sumr>0,]
+fit=LDA(docs3,k=12,method="GIBBS",control=list(keep=10000,thin=1,iter=5000,burnin=2500)) # estima el modelo LDA 
+terms=posterior(fit)$terms #probabilidades de términos
+topics=posterior(fit)$topics # probabilidades de tópicos
+
